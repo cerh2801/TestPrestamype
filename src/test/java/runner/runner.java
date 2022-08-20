8213c86936ea984fda0ed861ba7a37c15d896132
cerh2802 <cerh2802@gmail.com>
2022-08-20 16:26:22 -0500

    them.

---
+++ src/test/java/runner/runner.java
@@ -0,0 +1,35 @@
+package runner;
+
+
+
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+public class runner {
+
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            plugin = {"json:target/build/cucumber.json"},
+            //junit = {"--step-notifications"},
+            features = {"src/test/resources/feature"},
+
+            glue = {"StepDefinition.registrarme"},
+            tags = "@registrame"
+    )
+    class CukesRunner {
+   }
+}
+
+
+/*//import cucumber.api.junit.Cucumber;
+//import org.junit.runner.RunWith;
+//@RunWith(Cucumber.class)
+//@Cucumber.Options(
+//        format={"pretty", "html:target/cucumber"},
+//        features="src/test/resources"
+//)
+//class CukesRunner {
+//}*/
